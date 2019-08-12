104dfed79c870d9803eadc1b9308a520eac8e006
molodtsovaav <molodtsovaav@gmail.com>
2019-08-12 13:04:09 +0400

    Init app, addclient entity

---
+++ app.rb
@@ -0,0 +1,16 @@
+#encoding: utf-8
+require 'rubygems'
+require 'sinatra'
+require 'sinatra/reloader'
+require 'sqlite3'
+require 'byebug'
+require 'sinatra/activerecord'
+
+set :database, "sqlite3: barbershop.db"
+class Client < ActiveRecord::Base
+
+end
+
+get '/' do
+	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>!!!"			
+end
